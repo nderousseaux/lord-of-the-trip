9f3e6c153179238ac5a03732ea9e2f609e17cb7b
Nemanja ALABIC <nemanja.alabic@etu.unistra.fr>
2021-04-01 15:34:58 +0000

    Hotfix crosingpoint

--- dev/backend/server/loftes/models/Segment.py
+++ dev/backend/server/loftes/models/Segment.py
@@ -8,7 +8,6 @@ from loftes.models import Base
 class Segment(Base):
     __tablename__ = "Segment"
     id = Column(Integer, primary_key=True)
-    name = Column(String(255))
     start_crossing_point_id = Column(Integer, ForeignKey("CrossingPoint.id"))
     start_crossing_point = relationship(
         "CrossingPoint", foreign_keys="Segment.start_crossing_point_id"
