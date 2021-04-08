11e585c844c3eeb38e57da2805d6c75d2637ece2
Nemanja ALABIC <nemanja.alabic@etu.unistra.fr>
2021-04-08 13:52:51 +0200

    hotfix[sum events.distance]

--- dev/backend/server/loftes/marshmallow_schema/EventSchema.py
+++ dev/backend/server/loftes/marshmallow_schema/EventSchema.py
@@ -1,16 +1,10 @@
 from loftes.models import Events
 
-from marshmallow import (
-    Schema,
-    fields,
-    pre_dump,
-    post_dump,
-    post_load,
-    pre_load
-)
+from marshmallow import Schema, fields, pre_dump, post_dump, post_load, pre_load
 
 import datetime, time
 import json
+
 
 class EventSchema(Schema):
     id = fields.Int()
@@ -19,18 +13,18 @@ class EventSchema(Schema):
     segment_id = fields.Int(load_only=True)
     duration = fields.Int()
     move_type = fields.Int()
-    event_date = fields.DateTime()   
+    event_date = fields.DateTime()
     distance = fields.Int()
-    
+
     # id_user_info = fields.Nested(lambda: UserSchema())
     # id_challenge = fields.Nested(lambda: ParcoursSchema())
     # id_segment = fields.Nested(lambda: SegmentSchema())
     # event_type = fields.Int()
-    #footstep = fields.Int()
+    # footstep = fields.Int()
 
-    # class Meta:
-    #     ordered = True
-    
+    class Meta:
+        ordered = True
+
     @post_load
     def make_crossing_point(self, data, **kwargs):
         return Events(**data)
