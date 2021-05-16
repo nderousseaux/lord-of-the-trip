83f8ba57a0c2c8495741c88d91410da958352020
Nemanja ALABIC <nemanja.alabic@etu.unistra.fr>
2021-05-16 16:16:56 +0000

    feat: auth for obstacles and events

--- conception/apidoc/api_project.js
+++ conception/apidoc/api_project.js
@@ -44,7 +44,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-16T13:13:27.098Z",
+    "time": "2021-05-16T16:13:38.998Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
