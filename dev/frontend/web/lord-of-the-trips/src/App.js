a0444cdf2950a187efc608875165c42ae80e7fb0
Nicolas Metzger <nicolas.metzger3@etu.unistra.fr>
2021-05-16 20:55:47 +0200

    Possibilité de s'authentifier

--- dev/frontend/web/lord-of-the-trips/src/App.js
+++ dev/frontend/web/lord-of-the-trips/src/App.js
@@ -1,6 +1,9 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { HashRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { AuthProvider, useAuth } from './authentication/auth';
+import Login from './authentication/Login';
+import Signup from './authentication/Signup';
 import AdminChallenges from './admin/challenges'
 import EditChallenge from './admin/editChallenge'
 import EditMap from './admin/editMap'
@@ -15,13 +18,23 @@ const queryClient  = new QueryClient({
   }
 });
 
-const App = () => {
-
+const Header = () => {
+  let { user, logout} = useAuth();
   return (
-    <HashRouter>
-      <QueryClientProvider client={queryClient}>
+    <>
       <Link to="/"> <h1>Lord of the trips</h1> </Link>
-      <hr />
+      {!user ? <> <Link to="/login"><b>Login</b></Link> or <Link to="/signup"><b>Signup</b></Link> </> : null}
+      {user ? <> Hello {user.first_name} {user.last_name}, or {user.pseudo} to call you with your pseudo <button onClick={logout}>Logout</button> </> : null}
+    </>
+  );
+};
+
+const Main = () => {
+  return (
+    <>
+      <AuthProvider>
+        <Header />
+        <hr />
         <Switch>
           <Route exact path="/">
             <AdminChallenges />
@@ -32,9 +45,25 @@ const App = () => {
           <Route path="/editmap/:id">
             <EditMap />
           </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
           <Redirect to="/" />
         </Switch>
         {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </AuthProvider>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <HashRouter>
+      <QueryClientProvider client={queryClient}>
+        <Main />
       </QueryClientProvider>
     </HashRouter>
   );
---
+++ dev/frontend/web/lord-of-the-trips/src/authentication/auth.js
@@ -0,0 +1,75 @@
+import { useState, useEffect, createContext, useContext } from 'react';
+import { useHistory } from "react-router-dom";
+import { checkStatus, urlPrefix } from '../api/fetchUtils';
+
+let AuthContext = createContext();
+
+export let AuthProvider = ({children}) => {
+  let [user, setUser] = useState(null);
+  let [userCheck, setUserCheck] = useState(false);
+
+  const history = useHistory();
+
+  let login = (email, password) => {
+    return fetch(`${urlPrefix}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(data => {
+      setUser(data.user);
+      window.localStorage.setItem('token', data.token);
+      history.push("/");
+    });
+  };
+
+  let logout = () => {
+    setUser(null);
+    window.localStorage.removeItem('token');
+    history.push("/");
+  }
+
+  let signup = (first_name, last_name, pseudo, email, password) => {
+    return fetch(`${urlPrefix}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ first_name, last_name, pseudo, email, password })
+    })
+    .then(checkStatus)
+    .then(() => {
+      history.push("/login");
+    });
+  };
+
+  useEffect(() => {
+    let token = window.localStorage.getItem('token');
+    fetch(`${urlPrefix}/whoami`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(user => {
+      setUser(user);
+      setUserCheck(true);
+    })
+    .catch(() => {
+      setUser(null);
+      setUserCheck(true);
+    });
+  }, []);
+
+  return !userCheck ? "Checking authentication..." :
+    <AuthContext.Provider value={{ user, login, logout, signup }}>
+      {children}
+    </AuthContext.Provider>
+};
+
+export let useAuth = () => useContext(AuthContext);
