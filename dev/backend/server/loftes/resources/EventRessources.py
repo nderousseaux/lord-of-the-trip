c3d00d5a9d74129d9264d480b80a8a9a77b564fe
SACHER MARILYN <marilyn.sacher@etu.unistra.fr>
2021-05-19 22:06:42 +0200

    Add function for distance traveled by user on challenge

--- dev/backend/server/loftes/resources/EventRessources.py
+++ dev/backend/server/loftes/resources/EventRessources.py
@@ -1,4 +1,6 @@
 from loftes.models import Event, Challenge, User, Segment, Obstacle, UserChallenge, EventType, DBSession
+from sqlalchemy import func
+
 import datetime
 
 def findAllEventForUserByChallenge(user_id,challenge_id):
@@ -13,6 +15,17 @@ def findAllEventForUserByChallenge(user_id,challenge_id):
         .all()
     ) 
     
+    return data
+
+def distance_Event_For_User_By_Challenge(user_id,challenge_id):
+
+    data = (
+        DBSession.query(func.sum(Event.distance).label('distance'))
+        .filter(Event.user_id == user_id)
+        .join(Segment,Event.segment_id==Segment.id)
+        .filter(Segment.challenge_id==challenge_id).first()
+    )
+
     return data
 
 def findLastEventForUserByChallenge(user_id,challenge_id):
