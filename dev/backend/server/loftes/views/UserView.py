eb2145f9a69e53ab0ead7cb26d6bb4f75c22f55c
SACHER MARILYN <marilyn.sacher@etu.unistra.fr>
2021-06-03 15:54:57 +0200

    Add user modification

--- dev/backend/server/loftes/views/UserView.py
+++ dev/backend/server/loftes/views/UserView.py
@@ -128,6 +128,92 @@ def get_all_subscribers(request):
 
     return response
 
+user_update = Service(
+    name="user_update", 
+    path="/user/update", 
+    cors_policy=cors_policy
+)
+
+@user_update.put()
+def update_user(request):
+
+    service_informations = ServiceInformations()
+    user = DBSession.query(User).filter(User.email == request.authenticated_userid).first()
+
+    # check if user is authenticated
+    if user != None:
+
+        query = DBSession.query(User).filter(User.id==user.id)
+        try:
+
+            query.update(UserSchema().check_json(request.json))
+            DBSession.flush()
+            response = service_informations.build_response(exception.HTTPNoContent)
+
+        except ValidationError as validation_error:
+            response = service_informations.build_response(
+                exception.HTTPBadRequest, None, str(validation_error)
+            )
+
+        except ValueError as value_error:
+            response = service_informations.build_response(
+                exception.HTTPBadRequest, None, str(value_error)
+            )
+
+        except PermissionError as pe:
+            response = service_informations.build_response(exception.HTTPUnauthorized)
+
+        except Exception as e:
+            response = service_informations.build_response(exception.HTTPInternalServerError)
+            logging.getLogger(__name__).warn("Returning: %s", str(e))
+    else:
+        response = service_informations.build_response(exception.HTTPUnauthorized)
+
+    return response
+
+user_patch = Service(
+    name="user_patch", 
+    path="/user/modify", 
+    cors_policy=cors_policy
+    )
+
+@user_patch.patch()
+def modify_user(request):
+
+    service_informations = ServiceInformations()
+
+    user = DBSession.query(User).filter(User.email == request.authenticated_userid).first()
+
+    # check if user is authenticated
+    if user != None:
+
+        query = DBSession.query(User).filter(User.id==user.id)
+        try:
+
+            query.update(UserSchema().check_json(request.json))
+            DBSession.flush()
+            response = service_informations.build_response(exception.HTTPNoContent)
+
+        except ValidationError as validation_error:
+            response = service_informations.build_response(
+                exception.HTTPBadRequest, None, str(validation_error)
+            )
+
+        except ValueError as value_error:
+            response = service_informations.build_response(
+                exception.HTTPBadRequest, None, str(value_error)
+            )
+
+        except PermissionError as pe:
+            response = service_informations.build_response(exception.HTTPUnauthorized)
+
+        except Exception as e:
+            response = service_informations.build_response(exception.HTTPInternalServerError)
+            logging.getLogger(__name__).warn("Returning: %s", str(e))
+    else:
+        response = service_informations.build_response(exception.HTTPUnauthorized)
+
+    return response
 
 
 
