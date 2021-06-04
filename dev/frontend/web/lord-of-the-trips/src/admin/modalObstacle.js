77f7a5cd8771e0c0429cb3f4702cfaccf811b376
Nicolas Metzger <nicolas.metzger3@etu.unistra.fr>
2021-06-04 11:11:04 +0200

    Obstacles photo

--- dev/frontend/web/lord-of-the-trips/src/admin/modalObstacle.js
+++ dev/frontend/web/lord-of-the-trips/src/admin/modalObstacle.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useQueryClient, useMutation } from 'react-query';
 import apiObstacles from '../api/obstacles';
 import Button from '@material-ui/core/Button';
@@ -8,14 +8,27 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 const ModalObstacle = ({ obstacleObject, challengeId, openState, setOpenState }) => {
+  const [radioValue, setRadioValue] = useState(null);
   const [label, setLabel] = useState(null);
   const [description, setDescription] = useState(null);
   const [progress, setProgress] = useState(null);
-  const [nb_points, setNb_points] = useState(null);
   const [result, setResult] = useState(null);
   const queryClient = useQueryClient();
+
+  useEffect(() => {
+    setLabel(obstacleObject.label);
+    setDescription(obstacleObject.description);
+    setProgress(obstacleObject.progress * 100);
+    setResult(obstacleObject.result);
+    setRadioValue(obstacleObject.question_type.toString());
+  }, []);
 
   const updateObstacleMutation = useMutation( (obstacle) => apiObstacles.updateObstacle(obstacleObject.segmentId, obstacle, obstacleObject.id), {
     onSuccess: () => {
@@ -30,10 +43,10 @@ const ModalObstacle = ({ obstacleObject, challengeId, openState, setOpenState })
 
   const handleSubmit = () => {
     const obstacle = {};
+    obstacle.question_type = parseInt(radioValue);
     if(label) obstacle.label = label;
     if(description) obstacle.description = description;
-    if(progress && progress >= 1 && progress <= 99) obstacle.progress = progress / 100;
-    if(nb_points && nb_points > 0 && Number.isInteger(parseFloat(nb_points))) obstacle.nb_points = nb_points;
+    if(progress && progress >= 1 && progress <= 99) obstacle.progress = progress / 100; //Number.isInteger(parseFloat(progress))
     if(result) obstacle.result = result;
     updateObstacleMutation.mutate(obstacle);
   };
@@ -43,30 +56,24 @@ const ModalObstacle = ({ obstacleObject, challengeId, openState, setOpenState })
       <Dialog open={openState} onClose={closeModal} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Obstacle "{obstacleObject.label}"</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            <b>Current information : </b> <br />
-            - Question : {obstacleObject.label} <br />
-            - Description : {obstacleObject.description} <br />
-            - Progress : {obstacleObject.progress * 100}% <br />
-            - Number points : {obstacleObject.nb_points} <br />
-            - Result : {obstacleObject.result}
-          </DialogContentText>
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Type de l'obstacle</FormLabel>
+            <RadioGroup row aria-label="position" name="position" value={radioValue} onChange={e => setRadioValue(e.target.value)}>
+              <FormControlLabel value="0" control={<Radio />} label="Question" />
+              <FormControlLabel value="1" control={<Radio />} label="Photo" />
+            </RadioGroup>
+          </FormControl>
           <hr />
-          <form onSubmit={handleSubmit}>
-            <DialogContentText>
-              <b>Update information : </b>
-            </DialogContentText>
-            <TextField variant="outlined" margin="dense" type="text" label="New question" value={label} onChange={e => setLabel(e.target.value)} fullWidth />
-            <TextField variant="outlined" margin="dense" type="text" label="New description" value={description} onChange={e => setDescription(e.target.value)} fullWidth />
-            <TextField variant="outlined" margin="dense" type="number" label="New progress" value={progress} onChange={e => setProgress(e.target.value)} fullWidth
-                       error={progress !== null && (progress < 1 || progress > 99) ? true : false} />
-            <DialogContentText>Progress is the percentage of progress for the obstacle on the segment, accepted value is between 1 and 99</DialogContentText>
-            <TextField variant="outlined" margin="dense" type="number" label="New number points" value={nb_points} onChange={e => setNb_points(e.target.value)} fullWidth
-                       error={nb_points !== null && (nb_points <= 0 || !Number.isInteger(parseFloat(nb_points))) ? true : false} />
-            <DialogContentText>Number of points for the obstacle, accepted value is a positive integer</DialogContentText>
-            <TextField variant="outlined" margin="dense" type="text" label="New result" value={result} onChange={e => setResult(e.target.value)} fullWidth />
-          </form>
+
+          {radioValue === "0" ? <ObstacleQuestion updateObstacleMutation={updateObstacleMutation} label={label} setLabel={setLabel} description={description} setDescription={setDescription}
+                                progress={progress} setProgress={setProgress} result={result} setResult={setResult} handleSubmit={handleSubmit} />
+                              : null}
+          {radioValue === "1" ? <ObstaclePhoto updateObstacleMutation={updateObstacleMutation} label={label} setLabel={setLabel} description={description} setDescription={setDescription}
+                                progress={progress} setProgress={setProgress} handleSubmit={handleSubmit} />
+                              : null}
+
         </DialogContent>
+
         <DialogActions>
           <Button onClick={closeModal} color="primary">
             Cancel
@@ -78,6 +85,40 @@ const ModalObstacle = ({ obstacleObject, challengeId, openState, setOpenState })
       </Dialog>
     </div>
   );
+};
+
+const ObstacleQuestion = ({ updateObstacleMutation, label, setLabel, description, setDescription, progress, setProgress, result, setResult, handleSubmit }) => {
+
+  return <>
+
+    <form onSubmit={handleSubmit}>
+      <DialogContentText>
+        <b>Update information : </b>
+      </DialogContentText>
+      <TextField variant="outlined" margin="dense" type="text" label="New question" value={label} onChange={e => setLabel(e.target.value)} fullWidth />
+      <TextField variant="outlined" margin="dense" type="text" label="New description" value={description} onChange={e => setDescription(e.target.value)} fullWidth />
+      <TextField variant="outlined" margin="dense" type="number" label="New progress" value={progress} onChange={e => setProgress(e.target.value)} fullWidth
+                 error={progress !== null && (progress < 1 || progress > 99) ? true : false} />
+      <DialogContentText>Progress is the percentage of progress for the obstacle on the segment, accepted value is between 1 and 99</DialogContentText>
+      <TextField variant="outlined" margin="dense" type="text" label="New result" value={result} onChange={e => setResult(e.target.value)} fullWidth />
+    </form>
+  </>
+};
+
+const ObstaclePhoto = ({ updateObstacleMutation, label, setLabel, description, setDescription, progress, setProgress, handleSubmit }) => {
+
+  return <>
+    <form onSubmit={handleSubmit}>
+      <DialogContentText>
+        <b>Update information : </b>
+      </DialogContentText>
+      <TextField variant="outlined" margin="dense" type="text" label="New question" value={label} onChange={e => setLabel(e.target.value)} fullWidth />
+      <TextField variant="outlined" margin="dense" type="text" label="New description" value={description} onChange={e => setDescription(e.target.value)} fullWidth />
+      <TextField variant="outlined" margin="dense" type="number" label="New progress" value={progress} onChange={e => setProgress(e.target.value)} fullWidth
+                 error={progress !== null && (progress < 1 || progress > 99) ? true : false} />
+      <DialogContentText>Progress is the percentage of progress for the obstacle on the segment, accepted value is between 1 and 99</DialogContentText>
+    </form>
+  </>
 };
 
 export default ModalObstacle;
