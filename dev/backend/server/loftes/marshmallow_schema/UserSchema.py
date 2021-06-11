d325c6261c10444dffd86464564546490f5154fb
SACHER MARILYN <marilyn.sacher@etu.unistra.fr>
2021-06-11 17:15:58 +0200

    Fix bug user creation

--- dev/backend/server/loftes/marshmallow_schema/UserSchema.py
+++ dev/backend/server/loftes/marshmallow_schema/UserSchema.py
@@ -65,22 +65,22 @@ class UserSchema(Schema):
     @pre_load
     def pre_load(self, data, many, **kwargs):
 
-        # if "email" in data:
-        #     user = DBSession().query(User).filter_by(email=data["email"]).first()
+        if "email" in data:
+            user = DBSession().query(User).filter_by(email=data["email"]).first()
 
-        #     if user != None:
-        #         raise ValueError("This email is already in use. Please use another one.")
+            if user != None:
+                raise ValueError("This email is already in use. Please use another one.")
 
-        # if "pseudo" in data:
-        #     if not re.match("^[A-Za-z0-9_-]*$", data["pseudo"]):
-        #         raise ValueError("Pseudo can contain only letters, numbers and underscores.")
+        if "pseudo" in data:
+            if not re.match("^[A-Za-z0-9_-]*$", data["pseudo"]):
+                raise ValueError("Pseudo can contain only letters, numbers and underscores.")
 
-        #     user = DBSession().query(User).filter_by(pseudo=data["pseudo"]).first()
+            user = DBSession().query(User).filter_by(pseudo=data["pseudo"]).first()
 
-        #     if user != None:
-        #         raise ValueError("This pseudo is already in use. Please use another one.")
+            if user != None:
+                raise ValueError("This pseudo is already in use. Please use another one.")
 
-        data = UserResources().check_data(data)
+        # data = UserResources().check_data(data)
 
         return data
 
